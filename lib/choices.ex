16e9015b29988d4ca1e487916f30784446782785
Bruce <brucepark0@gmail.com>
2016-11-11 07:10:53 -0500

    Finish CLI app with choices

--- lib/choices.ex
+++ lib/choices.ex
@@ -1,2 +1,24 @@
 defmodule Choices do
+  def main(args) do
+    args |> parse_choices |> output
+  end
+
+  defp output(opts) do
+    case opts[:choice] do
+      "Blue" ->
+        IO.puts "You pulled the blue lever. That is progressive."
+      "Red" ->
+        IO.puts "You pulled the red lever. That is conservative."
+      _ ->
+        IO.puts "You clearly want to waste your choice. That's ok, it's a free country."
+    end
+  end
+
+  defp parse_choices(args) do
+    {options, _, _} = OptionParser.parse(args,
+      switches: [choice: :string]
+    )
+    IO.inspect options
+    options
+  end
 end
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Choices.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     escript: [main_module: Choices], # <- add this line
      deps: deps()]
   end
 
